ca1ccc24e96fd716fd498e2748820ef76998dcf9
abhilashshivarchaka <abilashshivarchaka@gmail.com>
2025-08-04 10:31:19 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index', { title: 'My Web App' });
+});
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
